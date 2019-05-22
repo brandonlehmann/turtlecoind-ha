e421ae3f0462de8abd464a70841f3a78d54841fb
TheDevMinerTV <tobigames200@gmail.com>
2019-05-22 20:09:59 +0200

    Add pm2 custom metrics

--- service.js
+++ service.js
@@ -6,6 +6,7 @@
 
 const TurtleCoind = require('./')
 const util = require('util')
+const pm2Metrics = require('@pm2/io')
 
 var daemon = new TurtleCoind({
   loadCheckpoints: './checkpoints.csv'
@@ -16,37 +17,74 @@ function log (message) {
   console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
 }
 
+function setupMetrics() {
+  let metrics = []
+  for (let [ name, unit ]  of [ [ "Status", false ], [ "Progress", "percent"], [ "Blockheight", "blocks" ], [ "Net hash", "h/s" ], [ "Difficulty", false ] ]) {
+    metrics.push(pm2Metrics.metric({
+      name: name,
+      unit: unit,
+    }))
+  }
+  return metrics
+}
+
+function resetMetrics(metrics) {
+  for (let metric of metrics) {
+    metric.set(undefined)
+  }
+}
+
 daemon.on('start', (args) => {
   log(util.format('TurtleCoind has started... %s', args))
+  resetMetrics(metrics)
+  customMetrics[0].set('starting');
 })
 
 daemon.on('started', () => {
   log('TurtleCoind is attempting to synchronize with the network...')
+  resetMetrics(metrics)
+  metrics[0].set('started');
 })
 
 daemon.on('syncing', (info) => {
   log(util.format('TurtleCoind has synchronized %s out of %s blocks [%s%]', info.height, info.network_height, info.percent))
+  resetMetrics(metrics)
+  metrics[0].set('synchronizing')
+  metrics[1].set(`${info.height}/${info.network_height} (${info.percent}%)`)
 })
 
 daemon.on('synced', () => {
   log('TurtleCoind is synchronized with the network...')
+  resetMetrics(metrics)
+  metrics[0].set('synchronized')
 })
 
 daemon.on('ready', (info) => {
   log(util.format('TurtleCoind is waiting for connections at %s @ %s - %s H/s', info.height, info.difficulty, info.globalHashRate))
+  metrics[0].set('waiting for connections')
+  metrics[2].set(info.height)
+  metrics[3].set(info.globalHashRate)
+  metrics[4].set(info.difficulty)
 })
 
 daemon.on('desync', (daemon, network, deviance) => {
   log(util.format('TurtleCoind is currently off the blockchain by %s blocks. Network: %s  Daemon: %s', deviance, network, daemon))
+  resetMetrics(metrics)
+  metrics[0].set('desynchronized')
+  metrics[1].set(`${info.daemon}/${info.network}`)
 })
 
 daemon.on('down', () => {
   log('TurtleCoind is not responding... stopping process...')
+  resetMetrics(metrics)
+  metrics[0].set('down')
   daemon.stop()
 })
 
 daemon.on('stopped', (exitcode) => {
   log(util.format('TurtleCoind has closed (exitcode: %s)... restarting process...', exitcode))
+  resetMetrics(metrics)
+  metrics[0].set(`stopped (code: ${exitcode})`)
   daemon.start()
 })
 
@@ -58,4 +96,5 @@ daemon.on('error', (err) => {
   log(err)
 })
 
+let metrics = setupMetrics()
 daemon.start()
